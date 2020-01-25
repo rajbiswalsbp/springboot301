356c0394c5afc52c70a42be2b607ca5b53b2d3db
RAJ GOPAL BISWAL <rajbiswal@gmail.com>
2020-01-25 05:20:03 +0000

    zuul code committed

---
+++ orderingservice/src/main/java/com/eatza/order/exception/ItemNotFoundException.java
@@ -0,0 +1,13 @@
+package com.eatza.order.exception;
+
+public class ItemNotFoundException extends Exception {
+	
+	
+	public ItemNotFoundException() {
+		super();
+	}
+	public ItemNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
